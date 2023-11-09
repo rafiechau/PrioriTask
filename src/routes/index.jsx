1cb8deeff76775babaece1dadc0e976b3cf6f4de
alfianElsa14 <alfian.elsa@phincon.com>
2023-11-09 15:46:56 +0700

    commit ajah

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,13 @@
 import MainLayout from '@layouts/MainLayout';
 
 import Home from '@pages/Home';
+import Login from '@pages/Login';
 import NotFound from '@pages/NotFound';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     protected: true,
     component: Home,
     layout: MainLayout,
@@ -16,36 +17,37 @@ const routes = [
     name: 'login',
     protected: false,
     component: Login,
-    layout: MainLayout,
+    // layout: MainLayout,
   },
-  {
-    path: '/register',
-    name: 'register',
-    protected: false,
-    component: Register,
-    layout: MainLayout,
-  },
-  {
-    path: '/add',
-    name: 'add',
-    protected: false,
-    component: Add,
-    layout: MainLayout,
-  },
-  {
-    path: '/edit',
-    name: 'edit',
-    protected: false,
-    component: Edit,
-    layout: MainLayout,
-  },
-  {
-    path: '/detail/:id',
-    name: 'detail',
-    protected: false,
-    component: Detail,
-    layout: MainLayout,
-  }
+  // {
+  //   path: '/register',
+  //   name: 'register',
+  //   protected: false,
+  //   component: Register,
+  //   layout: MainLayout,
+  // },
+  // {
+  //   path: '/add',
+  //   name: 'add',
+  //   protected: false,
+  //   component: Add,
+  //   layout: MainLayout,
+  // },
+  // {
+  //   path: '/edit',
+  //   name: 'edit',
+  //   protected: false,
+  //   component: Edit,
+  //   layout: MainLayout,
+  // },
+  // {
+  //   path: '/detail/:id',
+  //   name: 'detail',
+  //   protected: false,
+  //   component: Detail,
+  //   layout: MainLayout,
+  // },
+
   { path: '*', name: 'Not Found', component: NotFound, layout: MainLayout, protected: false },
 ];
 
